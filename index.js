a781975e0d70e4981c9abde7dac1c5c182217224
Aldemir Souza <aldemmir17b@hotmail.com>
2020-12-07 21:33:32 -0300

    Exercicio concluido

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const cenario1 = [0, 0, 1, 0, 0, 1, 0];
 const cenario2 = [0, 0, 0, 0, 1, 0];
 
-const jumpingOnClouds = c => {
 
+const jumpingOnClouds = c => {
+    let jumps = 0;
+    let i = 0;
+
+    while(i < c.length - 1){
+        if((i + 2 < c.length) && c[i + 2] == 0){
+            i +=2;
+            console.log("Pulou 2 nuvens", jumps);
+        }else{
+            i++;
+            console.log("Pulou 1 nuvem", jumps);
+        }
+        jumps++;
+    }
+
+    return jumps;
 }
 
-jumpingOnClouds(cenario1)
-
+jumpingOnClouds(cenario1);
 //c representa o cenario, mantenha o nome pra eu poder testar no hackerrank :D
